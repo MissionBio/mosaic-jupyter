a80407654801f48be12bb50056b7409a78e107bd
Saurabh Parikh <saurabh.parikh@missionbio.com>
2021-02-09 14:44:01 -0800

    Move streamlit to mosaic-streamlit

--- streamlit/build/run_mosaic.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from streamlit import cli as stcli
-
-if __name__ == '__main__':
-    launchdir = os.path.dirname(sys.argv[0])
-    sys.argv = ["streamlit", "run", f"{launchdir}/mosaic.py", "--global.developmentMode=false"]
-    sys.exit(stcli.main())
